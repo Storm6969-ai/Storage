217fb1169fda4b7a54bce9e9fdaedd57c07bb22a
Storm6969-ai <audriuszaveckas985@gmail,com>
2026-03-20 11:01:04 +0200

    Continue

--- DarbųSandelis/A.Z 2026.02.18 - RE7 DLC '21'/script.js
+++ DarbųSandelis/A.Z 2026.02.18 - RE7 DLC '21'/script.js
@@ -1,9 +1,9 @@
-function getRandomInt(min, max) {
+function getRandomInt(min, max) { // SPEED UP FUNCTION: gets a random number from the biggest and smallest num of an array
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function remove(array, value){ //speed up function: draws a card from the current deck, if card isn't there, nothing happens
+function remove(array, value){ //SPEED UP FUNCTION: removes a value from an array
   if (array.includes(value)){
     array.splice(array.indexOf(value), 1);
     return array;
@@ -11,7 +11,7 @@ function remove(array, value){ //speed up function: draws a card from the curren
     return false
   }
 }
-function drawCard(card = currentCards[getRandomInt(0, currentCards.length-1)]){ //speed up function: draws a card from the current deck, if card isn't there, nothing happens
+function drawCard(card = currentCards[getRandomInt(0, currentCards.length-1)]){ //SPEED UP FUNCTION: draws a card from the current deck, if card isn't there, nothing happens
   if (currentCards.includes(card)){
     remove(currentCards, card)
     return card
@@ -19,48 +19,30 @@ function drawCard(card = currentCards[getRandomInt(0, currentCards.length-1)]){ 
     return false
   }
 }
-let whichPlayerTrumpCard = playerTrumpCards[getRandomInt(0, playerTrumpCards.length-1)]
 
+// let whichPlayerTrumpCard = playerTrumpCards[getRandomInt(0, playerTrumpCards.length-1)]
+
+
+var playedByPlayer = true
 
 var playerTrumpCards = []
+var playerCards = 0
 var enemyTrumpCards = []
+var enemyCards = 0
 var playerRisk = 0
 var enemyRisk = 0
 var goal = 21
-var deckCards = ['card1', 'card2', 'card3', 'card4', 'card5', 'card6', 'card7', 'card8', 'card9', 'card10', 'card11']
+const deckCards = ['card1', 'card2', 'card3', 'card4', 'card5', 'card6', 'card7', 'card8', 'card9', 'card10', 'card11']
 var currentCards
 
 function deckReset(){currentCards = deckCards}
 deckReset()
 
-function giveCard() {
-  const randomCard = getRandomInt(0, currentCards.length-1)
-  return currentCards[randomCard]
-}
 
-
-
-function drawCard(card){ //speed up function: draws a card from the current deck, if card isn't there, nothing happens
-  if (currentCards.includes(card)){
-    remove(currentCards, card)
-    return card
-  } else {
-    return false
-  }
-}
-function removePlayerTC(){ // speed up function: removes trump card from player
-  
-  return playerTrumpCards
-}
-
-function drawTrumpCard(){
-  const i = getRandomInt(0, trumpCards.length - 1)
-  return trumpCards[i]
-}
+function drawTrumpCard(){return trumpCards[getRandomInt(0, trumpCards.length - 1)]}
 
 
 function trumpCardFunction_raiseRisk(raiseRiskBy){
-  let playedByPlayer = false   // change later
   if (playedByPlayer) {
     enemyRisk += raiseRiskBy
   } else {
@@ -79,38 +61,56 @@ const trumpCard_5Card = function trumpCard_5Card(){return drawCard('card5')}
 const trumpCard_6Card = function trumpCard_6Card(){return drawCard('card6')}
 const trumpCard_7Card = function trumpCard_7Card(){return drawCard('card7')}
 
-const trumpCard_TrumpSwitch = function trumpCard_TrumpSwitch(){
-  if (playedByPlayer) {
-    let removableTrumpCard = playerTrumpCards[getRandomInt(0, playerTrumpCards.length-1)]
-    x = remove(playerTrumpCards, removableTrumpCard)
-    console.log('Removed:', x)
-    remove(playerTrumpCards, removableTrumpCard)
-    playerTrumpCards.push(trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)])
-  } else {
-    remove(enemyTrumpCards, getRandomInt(0, enemyTrumpCards.length-1))
-    remove(enemyTrumpCards, getRandomInt(0, enemyTrumpCards.length-1))
-    enemyTrumpCards.push(trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)])
-  }
-}
+// const trumpCard_TrumpSwitch = function trumpCard_TrumpSwitch(){
+//   if (playedByPlayer) {
+//     let removableTrumpCard = playerTrumpCards[getRandomInt(0, playerTrumpCards.length-1)]
+//     remove(playerTrumpCards, removableTrumpCard)
+//     remove(playerTrumpCards, removableTrumpCard)
+//     playerTrumpCards.push(trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)])
+//   } else {
+//     remove(enemyTrumpCards, getRandomInt(0, enemyTrumpCards.length-1))
+//     remove(enemyTrumpCards, getRandomInt(0, enemyTrumpCards.length-1))
+//     enemyTrumpCards.push(trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)])
+//   }
+// }
 
-const trumpCard_TrumpSwitchPLUS = function trumpCard_TrumpSwitchPLUS(){
-  if (playedByPlayer) {
-    remove(playerTrumpCards, getRandomInt(0, playerTrumpCards.length-1))
-    playerTrumpCards.push(trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)])
-  } else {
-    remove(enemyTrumpCards, getRandomInt(0, enemyTrumpCards.length-1))
-    enemyTrumpCards.push(trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)])
-  }
-}
+// const trumpCard_TrumpSwitchPLUS = function trumpCard_TrumpSwitchPLUS(){
+//   if (playedByPlayer) {
+//     remove(playerTrumpCards, getRandomInt(0, playerTrumpCards.length-1))
+//     playerTrumpCards.push(trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)])
+//   } else {
+//     remove(enemyTrumpCards, getRandomInt(0, enemyTrumpCards.length-1))
+//     enemyTrumpCards.push(trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)], trumpCards[getRandomInt(0, trumpCards.length-1)])
+//   }
+// }
 
 
 
 const trumpCard_Shield = function trumpCard_Shield(){canTrumpCardBeOnTable(true), trumpCardFunction_raiseRisk(-1)} // add On Table effect
 const trumpCard_ShieldPLUS = function trumpCard_ShieldPLUS(){canTrumpCardBeOnTable(true), trumpCardFunction_raiseRisk(-2)} // add On Table effect
 
-const trumpCard_GoFor17 = function trumpCard_GoFor17(){goal = 17}
-const trumpCard_GoFor24 = function trumpCard_GoFor24(){goal = 24}
-const trumpCard_GoFor27 = function trumpCard_GoFor27(){goal = 27}
+const trumpCard_PerfectDraw = function trumpCard_PerfectDraw(){
+  var x
+  if (playedByPlayer){
+    x = goal - playerCards
+  } else {
+    x = goal - enemyCards
+  }
+  if (x > 11) {
+    x = currentCards[-1]
+  }
+  console.log(`card${x}`)
+}
+const trumpCard_PerfectDrawPLUS = function trumpCard_PerfectDrawPLUS(){
+
+}
+const trumpCard_UltimateDraw = function trumpCard_PerfectDraw(){
+
+}
+
+const trumpCard_GoFor17 = function trumpCard_GoFor17(){canTrumpCardBeOnTable(true), goal = 17}
+const trumpCard_GoFor24 = function trumpCard_GoFor24(){canTrumpCardBeOnTable(true), goal = 24}
+const trumpCard_GoFor27 = function trumpCard_GoFor27(){canTrumpCardBeOnTable(true), goal = 27}
 
 
 function repeat(item, count) {
@@ -203,8 +203,10 @@ var trumpCards = (
 // // console.log(trumpCard_7Card())
 // // console.log(currentCards)
 // trumpCard_TrumpSwitch()
-console.log(playerTrumpCards)
-playerTrumpCards.push(drawTrumpCard, drawTrumpCard, drawTrumpCard)
+// console.log(playerTrumpCards.length)
+
+// playerTrumpCards.push(drawTrumpCard, drawTrumpCard, drawTrumpCard)
 
 
-console.log(playerTrumpCards)
+// console.log(playerTrumpCards.length)
+console.log(trumpCard_PerfectDraw())
